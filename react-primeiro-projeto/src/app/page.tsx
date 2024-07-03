f5f7d7dbfb1556bbea6b19b692f72eedca0fa751
gellado <lima.mt96@gmail.com>
2024-07-03 18:52:57 -0300

    subindo geoForm

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Circle, Square } from "@/components/GeoIcons";
-
+import { GeoForm } from "@/components/GeoForm";
 const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl"> Olá Mundo</h1>
       <h3>Texto Complementar</h3>
-      <Square/>
-      <Circle/>
+      <GeoForm/>
     </div>
   );
 }
